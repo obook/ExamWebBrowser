4bf7d344161c7d9169ce9259e3051408e62a1bd0
obook <obooklage@free.fr>
2024-05-15 16:40:03 +0200

    Updated

---
+++ distribution.bat
@@ -0,0 +1,4 @@
+del .\bin\ExamWebBrowser.exe
+xcopy .\build\ExamWebBrowser.exe .\bin\
+X:\Qt\6.7.0\msvc2019_64\bin\windeployqt X:\ExamWebBrowser\bin\ExamWebBrowser.exe
+tar.exe -a -cf ExamWebBrowser.zip bin
