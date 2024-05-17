1f6ac130c8197e909b7c93efd340219482d1b92c
obook <obooklage@free.fr>
2024-05-17 23:27:21 +0200

    Build for Windows

--- distribution.bat
+++ distribution.bat
@@ -1,5 +1,7 @@
-del .\bin\ExamWebBrowser.exe
-xcopy .\build\ExamWebBrowser.exe .\bin\
-X:\Qt\6.7.0\msvc2019_64\bin\windeployqt X:\ExamWebBrowser\bin\ExamWebBrowser.exe
+mkdir .\bin\Windows\
+del .\bin\Windows\ExamWebBrowser.exe
+xcopy .\src\build\Desktop_Qt_6_7_0_MSVC2019_64bit-Release\ExamWebBrowser.exe .\bin\Windows\
+xcopy .\media\ExamWebBrowser.png .\bin\Windows\
+X:\Qt\6.7.0\msvc2019_64\bin\windeployqt X:\ExamWebBrowser\bin\Windows\ExamWebBrowser.exe
 tar.exe -a -cf ExamWebBrowser.zip bin
 "X:\Program Files (x86)\NSIS\makensis.exe" ExamWebBrowser.nsi
