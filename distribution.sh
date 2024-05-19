ca59de1f954688e5223f896c4b44e8cc871e4b71
obook <obooklage@free.fr>
2024-05-19 15:56:12 +0200

    Updated

--- distribution.sh
+++ distribution.sh
@@ -1,7 +1,6 @@
-mkdir -p ./bin/linux/
-rm ./bin/linux/ExamWebBrowser
+rm -R ./bin/linux
+mkdir -p ./bin/linux
 cp ./build/Linux-Release/ExamWebBrowser ./bin/linux/
 cp ./media/ExamWebBrowser.png ./bin/linux/
+zip -r ExamWebBrowser_Linux.zip ./bin/linux
 # X:\Qt\6.7.0\msvc2019_64\bin\windeployqt X:\ExamWebBrowser\bin\ExamWebBrowser
-# tar.exe -a -cf ExamWebBrowser.zip bin
-# "X:\Program Files (x86)\NSIS\makensis.exe" ExamWebBrowser.nsi
