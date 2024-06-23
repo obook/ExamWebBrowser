06fbf9fd71672be47e53e0e65924d3d589badc94
obook <obooklage@free.fr>
2024-06-23 22:27:04 +0200

    Rename

--- distribution_Linux.sh
+++ distribution_Linux.sh
@@ -3,5 +3,5 @@ mkdir -p ./bin/linux/images
 cp ./src/build/Desktop_Qt_6_5_3_GCC_64bit-Release/ExamWebBrowser ./bin/linux/
 cp ./images/ExamWebBrowser.png ./bin/linux/images
 cp ./images/home.svg ./bin/linux/images
-zip -r ExamWebBrowser_Linux.zip ./bin/linux
+zip -r ./bin/ExamWebBrowser_Linux.zip ./bin/linux
 # X:\Qt\6.7.0\msvc2019_64\bin\windeployqt X:\ExamWebBrowser\bin\ExamWebBrowser
