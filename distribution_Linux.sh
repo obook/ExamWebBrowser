20fbdf191460292787a645741430c547481d77a0
obook <obooklage@free.fr>
2024-06-24 10:43:26 +0200

    Update distribution_Linux.sh

--- distribution_Linux.sh
+++ distribution_Linux.sh
@@ -1,7 +1,10 @@
-rm -R ./bin/linux
-mkdir -p ./bin/linux/images
-cp ./src/build/Desktop_Qt_6_5_3_GCC_64bit-Release/ExamWebBrowser ./bin/linux/
-cp ./images/ExamWebBrowser.png ./bin/linux/images
-cp ./images/home.svg ./bin/linux/images
-zip -r ./bin/ExamWebBrowser_Linux.zip ./bin/linux
-# X:\Qt\6.7.0\msvc2019_64\bin\windeployqt X:\ExamWebBrowser\bin\ExamWebBrowser
+rm -R ./bin/ExamWebBrowser_linux
+rm -R ./distribution/
+mkdir -p ./bin/ExamWebBrowser_linux/images
+mkdir -p ./bin/ExamWebBrowser_linux/images
+mkdir -p ./distribution
+cp ./src/build/Desktop_Qt_6_5_3_GCC_64bit-Release/ExamWebBrowser ./bin/ExamWebBrowser_linux/
+cp ./images/ExamWebBrowser.png ./bin/ExamWebBrowser_linux/images
+cp ./images/home.svg ./bin/ExamWebBrowser_linux/images
+cd ./bin
+zip -r ./../distribution/ExamWebBrowser_Linux.zip ./ExamWebBrowser_linux
